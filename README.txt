5293b4c59007a91a81482d4ccdef5be753880d2e
jcbazan <75187084+jcbazan@users.noreply.github.com>
2026-06-21 23:29:15 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,1 @@
+Starter package. Extend parser.js with Tesseract word-coordinate parsing.
--- parser.js
+++ parser.js
@@ -1,4 +1,9 @@
-// Replace with your Tesseract parser logic
-function extractFields(words){
- return {};
-}
+const FIELD_SCHEMA={
+order_number:'',bol_number:'',
+shipper:'',shipper_address:'',shipper_city:'',shipper_phone:'',
+consignee:'',consignee_address:'',consignee_city:'',consignee_phone:'',
+commodity:'',weight:'',length:'',width:'',empty_weight:'',
+loaded_miles:'',empty_miles:'',
+early_pickup:'',late_pickup:'',early_delivery:'',late_delivery:'',
+comments:''
+};
